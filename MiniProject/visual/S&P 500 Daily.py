79d6ed93d7782fd737b7326aa7408409e3296826
Yong Chen <73269209+YongjiangChen@users.noreply.github.com>
2022-10-06 20:38:06 +0100

    Delete S&P 500 Daily.py

--- MiniProject/visual/S&P 500 Daily.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-
-sp500 = yf.Ticker('^GSPC')
-sp500_hist = sp500.history(period='1d')
-#sp500_hist.to_csv('sp500_1y.csv')
-result = sp500_hist.to_json(orient="records")
-print(result)
